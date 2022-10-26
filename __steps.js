436d72fb23a1fbf2e5b7b1c7cd5041e9eec76587
Programming Hero <programming.hero1@gmail.com>
2022-10-26 13:21:35 -0500

    simple node server

---
+++ __steps.js
@@ -0,0 +1,34 @@
+/* 
+POST
+1. create post API
+2. use fetch with method POST
+3. add headers to the fetch: 'content-type': 'application/json'
+4. add body to the fetch to send data
+5. Make sure to use JSON.stringify to send data
+6. use express.json() as middle ware
+*/
+
+/* 
+Query: 
+https://www.youtube.com/watch?v=CSB8DohoFfA&ab_channel=GSeriesMusic
+path?name=value&name=value&name=value
+*/
+
+/* 
+1. sign up mongodb atlas
+2. dba user
+3. Whitelist your ip address
+4. database> connect> show entire code
+5. npm i mongodb
+*/
+
+/* 
+Insert: 
+
+*/
+
+/* 
+
+find: 
+
+*/
---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Simple Node Server Running');
+});
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com' },
+    { id: 2, name: 'Sabnoor', email: 'sabnoor@gmail.com' },
+    { id: 3, name: 'Sabila', email: 'sabila@gmail.com' },
+];
+
+// username: dbUser1
+// password: QkWg57etFE8CZjsw
+
+
+
+const uri = "mongodb+srv://dbUser1:QkWg57etFE8CZjsw@cluster0.onnrjmp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const userCollection = client.db('simpleNode').collection('users');
+        // const user = {name: 'Nahiya Mahi', email: 'nehi@gmail.com'}
+        // const result = await userCollection.insertOne(user);
+        // console.log(result);
+
+        app.get( '/users', async (req, res) =>{
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            user._id = result.insertedId;
+            res.send(user);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err =>console.log(err))
+
+
+
+// app.get('/users', (req, res) => {
+//     if(req.query.name){
+//         const search = req.query.name;
+//         const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0);
+//         res.send(filtered);
+//     }
+//     else {
+//         res.send(users);
+//     }
+    
+// });
+
+// app.post('/users', (req, res) => {
+//     const user = req.body;
+//     user.id = users.length + 1;
+//     users.push(user);
+//     console.log(user)
+//     res.send(user);
+// })
+
+app.listen(port, () => {
+    console.log(`Simple not server running on port ${port}`);
+})
